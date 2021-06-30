70d3a72bf64727b6dcc6ed7ddc01fb3d7ed129b4
ignaciocervino <ignaciocervino@gmail.com>
2021-06-29 22:37:30 -0300

    #04 Validating form

---
+++ js/app.js
@@ -0,0 +1,21 @@
+import * as UI from './interfaz.js';
+
+UI.formularioBuscar.addEventListener('submit',buscarCancion);
+
+function buscarCancion(e){
+    e.preventDefault();
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if (artista===''||cancion==='') {
+        //El usuario dejo almenos un campo vacio
+        UI.divMensajes.textContent = 'Error... Todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+
+        setTimeout(()=>{
+            UI.divMensajes.textContent='';
+            UI.divMensajes.classList.remove('error');
+        },3000);
+    }
+}
